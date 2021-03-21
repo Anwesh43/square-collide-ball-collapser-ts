e8c74d5e85f05834e8abac0ca7a396a653881662
Anwesh43 <anweshthecool0@gmail.com>
2021-03-21 12:41:40 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -29,3 +29,39 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawSquareCollideBallCollapser(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / squareSizeFactor 
+        const r : number = Math.min(w, h) / ballRFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const sf5 : number = ScaleUtil.divideScale(sf, 4, parts)
+        const squareH : number = size * (sf1 - sf5)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.fillRect(-w / 2 + (w /2 - size) * sf2, -squareH, size, squareH)
+            context.restore()
+        }
+        DrawingUtil.drawCircle(context, 0, -h / 2 + r + (h / 2 - size - 2 * r) * sf4 + size * sf5, r * sf3)
+        context.restore()
+    }
+
+    static drawSCBCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSquareCollideBallCollapser(context, scale)
+    }
+}
