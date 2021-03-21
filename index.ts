e400c21de9d8d61e62b5ace884e2cb0e44d92ea4
Anwesh43 <anweshthecool0@gmail.com>
2021-03-21 13:17:05 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -105,10 +105,11 @@ class State {
 
     scale : number = 0 
     dir : number = 0 
-    prevScale : number 
+    prevScale : number = 0
 
     update(cb : Function) {
         this.scale += scGap * this.dir 
+        console.log(this.scale)
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir 
             this.dir = 0 
